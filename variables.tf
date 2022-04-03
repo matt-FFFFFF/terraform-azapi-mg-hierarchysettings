6275b4f2e71169b8cd8f80c1b0fb0937e4be7ccb
Matt White <matt.white@microsoft.com>
2022-04-03 09:07:17 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,13 +10,13 @@ variable "tenant_root_group_name" {
 variable "update_existing" {
   type = bool
   default = false
-  description = "Enable this if the hierarchy settings object already exists and you want to manage it with this module."
+  description = "Enable this if the hierarchy settings object already exists and you want to manage it with this module without running `terraform import`."
 }
 
 variable "default_management_group_name" {
   type = string
   condition = can(regex("^[a-zA-Z0-9_-().]{90}$"))
-  description = "The name of the default management group. This is where all newly created subscriptions be placed unless specified otherwise."
+  description = "The name of the default management group. This is where all newly created subscriptions will be placed unless specified otherwise."
 }
 
 variable "" {
