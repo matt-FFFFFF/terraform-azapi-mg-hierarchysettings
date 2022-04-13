1028bb43ed505d7b10eb8ef82315cb6ae3fd1c5d
Matt White <matt.white@microsoft.com>
2022-04-13 17:02:47 +0100

    Fix variables

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "tenant_root_group_name" {
   type        = string
   description = "Usually the Azure AD tenant id - also the name of the tenant root management group."
   validation {
-    condition     = can(regex("^[a-f\\d]{4}(?:[a-f\\d]{4}-){4}[a-f\\d]{12}$", var.tenant_id))
+    condition     = can(regex("^[a-f\\d]{4}(?:[a-f\\d]{4}-){4}[a-f\\d]{12}$", var.tenant_root_group_name))
     error_message = "The value must be a UUID."
   }
 }
@@ -17,7 +17,7 @@ variable "default_management_group_name" {
   type        = string
   description = "The name of the default management group. This is where all newly created subscriptions will be placed unless specified otherwise."
   validation {
-    condition = can(regex("^[a-zA-Z0-9_-().]{90}$"))
+    condition     = can(regex("^[a-zA-Z0-9_-().]{90}$", var.default_management_group_name))
     error_message = "Name must be a maximum of 90 characters, consistying of: a-z, A-Z, 0-9, _, -, (, )."
   }
 }
